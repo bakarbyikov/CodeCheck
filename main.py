85820f38de7e29049fabda1982222f4ed51c1e59
DigmaBalls <DigmaBalls@gmail.com>
2025-03-20 20:32:27 +0500

    Added static to serving

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
 
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
 from model import Model
 
 app = FastAPI()
-model = Model()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def main():
+    return FileResponse("static/index.html")
 
 @app.get("/generate_problem")
 def generate_problem(text: str):
+    model = Model()
     solution = model.create_solution(text)
     tests = model.create_tests(text, solution)
     return {

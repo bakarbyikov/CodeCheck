2964e4e5cfde7e651b01d28b04eadcdaeaae9357
DigmaBalls <DigmaBalls@gmail.com>
2025-03-24 22:14:25 +0500

    Added problem loader and tester

--- model.py
+++ model.py
@@ -4,42 +4,43 @@ from llama_cpp import Llama
 
 
 class Model:
-    code_pattern = re.compile("```python(.*)```", re.DOTALL)
-    json_pattern = re.compile("```json(.*)```", re.DOTALL)
+    code_pattern = re.compile("```python([^`]*)", re.DOTALL)
+    json_pattern = re.compile("```json([^`]*)", re.DOTALL)
 
     def __init__(self):
         self.llm = Llama.from_pretrained(
-            repo_id="QuantFactory/Nxcode-CQ-7B-orpo-GGUF",
-            filename="Nxcode-CQ-7B-orpo.Q8_0.gguf",
+            repo_id="Qwen/Qwen2.5-Coder-0.5B-Instruct-GGUF",
+            filename="qwen2.5-coder-0.5b-instruct-fp16.gguf",
             verbose=False,
             n_gpu_layers=-1,
             n_threads=16,
-            n_ctx = 1<<9,
+            n_ctx=1 << 10,
         )
-        
-    
+
     def ask(self, text: str) -> str:
         response = self.llm.create_chat_completion(
             messages=[
-                {"role": "system", 
+                {"role": "system",
                  "content": "You are an expert AI coding assistant."},
                 {"role": "user", "content": text}
             ],
         )
         return response["choices"][0]["message"]["content"]
-    
+
     def extract_code(self, text: str) -> str:
+        print(text)
         found = self.code_pattern.search(text)
         return found and found.group(1).strip()
 
     def extract_json(self, text: str) -> str:
+        print(text)
         found = self.json_pattern.search(text)
         return found and found.group(1).strip()
-        
+
     def create_solution(self, problem: str) -> str:
         prompt = f"Solve next problem using python.\n###\n{problem}\n###\n"
         return self.extract_code(self.ask(prompt))
-    
+
     def create_tests(self, problem: str, code: str) -> str:
         prompt = 'List some testcases to fully test entire code using JSON schema:\n Testcase = {"input": str, "expected": str}\nReturn: List[Testcase]'
         prompt = "\n###\n".join((prompt, problem, code))
@@ -47,6 +48,16 @@ class Model:
         print(result)
         return self.extract_json(result) or self.extract_code(result)
 
+
 if __name__ == "__main__":
+    from tester import Problem, test
+    from contexttimer import Timer
+    from loguru import logger
+    
+    problems = Problem.from_folder("DataSet/Tests")
+    melons = problems["melons"]
     model = Model()
-    model.create_solution()
+    with Timer() as timer:
+        solution = model.create_solution(melons.text)
+    logger.info(f"Created colution in {timer.elapsed:.2f} seconds")
+    test(solution, melons)
---
+++ tester.py
@@ -0,0 +1,93 @@
+import subprocess
+from csv import DictReader
+from itertools import count
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+from typing import Self
+
+from loguru import logger
+from pydantic.dataclasses import dataclass
+
+
+@dataclass
+class Test:
+    input: str
+    expected: str
+    required: bool
+
+    @classmethod
+    def from_file(cls, path: Path) -> list[Self]:
+        with path.open() as file:
+            reader = DictReader(file)
+            return [cls(**row) for row in reader]
+
+
+@dataclass
+class Problem:
+    name: str
+    text: str
+    tests: list[Test]
+    path: str
+
+    @classmethod
+    def from_folder(cls, folder: str) -> dict[str, Self]:
+        found = dict()
+        for root, dirs, files in Path(folder).walk():
+            if not files:
+                continue
+
+            try:
+                tests = Test.from_file(root/"test.csv")
+            except FileNotFoundError:
+                logger.warning(f"Can't find text in {root}")
+
+            try:
+                text = (root/"text.txt").read_text()
+            except FileNotFoundError:
+                logger.warning(f"Can't find text in {root}")
+
+            try:
+                name = (root/"name.txt").read_text()
+            except FileNotFoundError:
+                name = root.name
+
+            if name in found:
+                for i in count(1):
+                    indexed = f"{name}_{i}"
+                    if indexed not in found:
+                        name = indexed
+                        break
+                logger.warning(
+                    f"Problem {name=} is not unique, replacing with {indexed}")
+
+            found[name] = cls(name, text, tests, str(root))
+        return found
+
+
+def run(solution: str, input: str) -> str:
+    programm = NamedTemporaryFile("w")
+    with programm as file:
+        file.write(solution)
+
+    return subprocess.run(["python", programm.name],
+                          input=input,
+                          text=True,
+                          capture_output=True).stdout
+
+
+def test(solution: str, problem: Problem):
+    passed = 0
+    for i, test in enumerate(problem.tests):
+        expected = test.expected
+        got = run(solution, test.input)
+        if got == test.expected:
+            logger.success(f"Test #{i} passed {expected=} {got=}")
+            passed += 1
+        else:
+            logger.error(f"Test #{i} failed {expected=} {got=}")
+    logger.info(f"Passed {passed}/{len(problem.tests)} tests")
+
+
+if __name__ == "__main__":
+    problems = Problem.from_folder("DataSet/Tests")
+    melons = problems["melons"]

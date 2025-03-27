aad1f51056aa5f71461ed443837295fab30e5c80
DigmaBalls <DigmaBalls@gmail.com>
2025-03-27 14:18:37 +0500

    Added server run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import uvicorn
 from fastapi import FastAPI
-
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
@@ -23,3 +23,5 @@ def generate_problem(text: str):
         "tests": tests
     }
 
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)

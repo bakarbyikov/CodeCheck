e0fab69baed6a47133fd2f1814e26d26ba2680f2
Iversy <iversy49@ya.ru>
2025-04-02 14:11:15 +0500

    len_four_odd_even smart

--- DataSet/Tests/Pavlova/ПЗ_2_ПиОА/len_four_odd_even/solution.py
+++ DataSet/Tests/Pavlova/ПЗ_2_ПиОА/len_four_odd_even/solution.py
@@ -1,9 +1,8 @@
 def solve(x: str) -> bool:
     if len(x) != 4 or int(x) < 0:
         return None
-    even: bool = any(int(i) % 2 == 0 for i in x)
-    odd: bool = any(int(i) % 2 for i in x)   
-    return even and odd
+    sm = sum(int(i) % 2 for i in x)
+    return 0 < sm < 4
 
 x: str = input()
 result = solve(x)
